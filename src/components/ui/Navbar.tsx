78eb5bbd85b5166b19ce69da65f34575a3119604
Zeeshan Haider <zeeshan.haider01110@gmail.com>
2024-05-28 21:26:25 +0500

    [FIX] ui for redirection of signin and signup pages

--- src/components/ui/Navbar.tsx
+++ src/components/ui/Navbar.tsx
@@ -11,9 +11,11 @@ import {
   Transition,
 } from "@headlessui/react";
 import { Bars3Icon, BellIcon, XMarkIcon } from "@heroicons/react/24/outline";
-import { signOut } from "next-auth/react";
+import { signOut, useSession } from "next-auth/react";
 import Image from "next/image";
 import archoLogo from "public/logo_resized.jpeg";
+import Button from "./Buttons/Button";
+import { useRouter } from "next/navigation";
 const navigation = [
   { name: "Home", href: "/", current: true },
   { name: "My Blogs", href: "myblogs", current: false },
@@ -24,6 +26,8 @@ function classNames(...classes: string[]) {
 }
 
 const Navbar = () => {
+  const { data } = useSession();
+  const router = useRouter();
   return (
     <Disclosure as="nav" className="bg-gray-800">
       {({ open }: { open: boolean }) => (
@@ -76,64 +80,86 @@ const Navbar = () => {
                 </div>
               </div>
               <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-                {/* Profile dropdown */}
-                <Menu as="div" className="relative ml-3">
-                  <div>
-                    <MenuButton className="relative flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
-                      <span className="absolute -inset-1.5" />
-                      <span className="sr-only">Open user menu</span>
-                      <Image
-                        className="h-8 w-8 rounded-full"
-                        src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                        alt=""
-                        width={0}
-                        height={0}
-                        unoptimized
-                      />
-                    </MenuButton>
-                  </div>
-                  <Transition
-                    enter="transition ease-out duration-100"
-                    enterFrom="transform opacity-0 scale-95"
-                    enterTo="transform opacity-100 scale-100"
-                    leave="transition ease-in duration-75"
-                    leaveFrom="transform opacity-100 scale-100"
-                    leaveTo="transform opacity-0 scale-95"
-                  >
-                    <MenuItems className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-                      <MenuItem>
-                        {({ focus }: { focus: boolean }) => (
-                          <a
-                            href="#"
-                            className={classNames(
-                              focus ? "bg-gray-100" : "",
-                              "block px-4 py-2 text-sm text-gray-700"
-                            )}
-                          >
-                            Your Profile
-                          </a>
-                        )}
-                      </MenuItem>
+                <span className="text-white text-sm">{data?.user?.name ?? ""}</span>
+                {data ? (
+                  <Menu as="div" className="relative ml-3">
+                    <div>
+                      <MenuButton className="relative flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
+                        <span className="absolute -inset-1.5" />
+                        <span className="sr-only">Open user menu</span>
+                        <Image
+                          className="h-8 w-8 rounded-full"
+                          src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                          alt=""
+                          width={0}
+                          height={0}
+                          unoptimized
+                        />
+                      </MenuButton>
+                    </div>
+                    <Transition
+                      enter="transition ease-out duration-100"
+                      enterFrom="transform opacity-0 scale-95"
+                      enterTo="transform opacity-100 scale-100"
+                      leave="transition ease-in duration-75"
+                      leaveFrom="transform opacity-100 scale-100"
+                      leaveTo="transform opacity-0 scale-95"
+                    >
+                      <MenuItems className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                        <MenuItem>
+                          {({ focus }: { focus: boolean }) => (
+                            <a
+                              href="#"
+                              className={classNames(
+                                focus ? "bg-gray-100" : "",
+                                "block px-4 py-2 text-sm text-gray-700"
+                              )}
+                            >
+                              Your Profile
+                            </a>
+                          )}
+                        </MenuItem>
 
-                      <MenuItem>
-                        {({ focus }: { focus: boolean }) => (
-                          <a
-                            href="#"
-                            className={classNames(
-                              focus ? "bg-gray-100" : "",
-                              "block px-4 py-2 text-sm text-gray-700"
-                            )}
-                            onClick={async () => {
-                              signOut();
-                            }}
-                          >
-                            Sign out
-                          </a>
-                        )}
-                      </MenuItem>
-                    </MenuItems>
-                  </Transition>
-                </Menu>
+                        <MenuItem>
+                          {({ focus }: { focus: boolean }) => (
+                            <a
+                              href="#"
+                              className={classNames(
+                                focus ? "bg-gray-100" : "",
+                                "block px-4 py-2 text-sm text-gray-700"
+                              )}
+                              onClick={async () => {
+                                signOut();
+                              }}
+                            >
+                              Sign out
+                            </a>
+                          )}
+                        </MenuItem>
+                      </MenuItems>
+                    </Transition>
+                  </Menu>
+                ) : (
+                  <div className="flex gap-2">
+                    <Button
+                      text="Signin"
+                      className="text-black text-sm bg-white"
+                      type="button"
+                      onClick={() => {
+                        router.push("/auth/signin");
+                      }}
+                    />
+                    <Button
+                      text="Signup"
+                      className="text-black text-sm bg-white"
+                      type="button"
+                      onClick={() => {
+                        router.push("/auth/signup");
+                      }}
+                    />
+                  </div>
+                )}
+                {/* Profile dropdown */}
               </div>
             </div>
           </div>
